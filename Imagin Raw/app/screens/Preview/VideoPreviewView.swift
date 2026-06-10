01495e3fc230584d3dc3cf470235309609f59ae4
Cristian Baluta <cristi.baluta@gmail.com>
2026-06-10 12:21:42 +0300

    Use AVPlayer to fix crashes on Tahoe

--- Imagin Raw/app/screens/Preview/VideoPreviewView.swift
+++ Imagin Raw/app/screens/Preview/VideoPreviewView.swift
@@ -7,34 +7,67 @@ import SwiftUI
 import AVKit
 import AVFoundation
 
-struct VideoPreviewView: View {
+struct VideoPreviewView: NSViewRepresentable {
     let photo: PhotoItem
 
-    @State private var player: AVPlayer
-
-    init(photo: PhotoItem) {
-        self.photo = photo
-        let p = AVPlayer(url: URL(fileURLWithPath: photo.path))
-        _player = State(initialValue: p)
+    func makeNSView(context: Context) -> AVPlayerView {
+        let playerView = AVPlayerView()
+        playerView.controlsStyle = .inline
+        playerView.player = AVPlayer(url: URL(fileURLWithPath: photo.path))
+        playerView.player?.play()
+        return playerView
     }
 
-    var body: some View {
-        VideoPlayer(player: player)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .onDisappear {
-            teardownPlayer()
+    func updateNSView(_ playerView: AVPlayerView, context: Context) {
+        // Only swap the player if the photo actually changed
+        let newURL = URL(fileURLWithPath: photo.path)
+        guard ((playerView.player?.currentItem?.asset as? AVURLAsset) != nil),
+              (playerView.player?.currentItem?.asset as? AVURLAsset)?.url != newURL
+        else {
+            return
         }
-        .onChange(of: photo) { _, newPhoto in
-            switchVideo(to: newPhoto)
-        }
+
+        playerView.player?.pause()
+        playerView.player = AVPlayer(url: newURL)
+        playerView.player?.play()
     }
 
-    private func teardownPlayer() {
-        player.pause()
-    }
-
-    private func switchVideo(to newPhoto: PhotoItem) {
-        teardownPlayer()
-        player = AVPlayer(url: URL(fileURLWithPath: newPhoto.path))
+    static func dismantleNSView(_ playerView: AVPlayerView, coordinator: ()) {
+        playerView.player?.pause()
+        playerView.player = nil
     }
 }
+
+// This crashes on Tahoe if the build is made from Sequoia
+
+//struct VideoPreviewView: View {
+//    let photo: PhotoItem
+//
+//    @State private var player: AVPlayer
+//
+//    init(photo: PhotoItem) {
+//        self.photo = photo
+//        let p = AVPlayer(url: URL(fileURLWithPath: photo.path))
+//        _player = State(initialValue: p)
+//    }
+//
+//    var body: some View {
+//        VideoPlayer(player: player)
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .onDisappear {
+//            teardownPlayer()
+//        }
+//        .onChange(of: photo) { _, newPhoto in
+//            switchVideo(to: newPhoto)
+//        }
+//    }
+//
+//    private func teardownPlayer() {
+//        player.pause()
+//    }
+//
+//    private func switchVideo(to newPhoto: PhotoItem) {
+//        teardownPlayer()
+//        player = AVPlayer(url: URL(fileURLWithPath: newPhoto.path))
+//    }
+//}
